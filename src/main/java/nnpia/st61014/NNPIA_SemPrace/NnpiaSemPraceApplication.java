e1947b021a08f3fab777960cb65c1468b353bbef
st61014 <st61014@upce.cz>
2023-04-10 20:21:15 +0200

    innit

---
+++ src/main/java/nnpia/st61014/NNPIA_SemPrace/NnpiaSemPraceApplication.java
@@ -0,0 +1,13 @@
+package nnpia.st61014.NNPIA_SemPrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnpiaSemPraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnpiaSemPraceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nnpia/st61014/NNPIA_SemPrace/NnpiaSemPraceApplicationTests.java
@@ -0,0 +1,13 @@
+package nnpia.st61014.NNPIA_SemPrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpiaSemPraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
